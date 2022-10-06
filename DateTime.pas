915e1fd3bcbbcb12f4b994858d5785fa76d0b2b6
Степунин Иван <ivstepunin@gmail.com>
2022-10-06 14:43:22 +0300

    сообщение коммита

---
+++ DateTime.pas
@@ -0,0 +1,18 @@
+﻿begin
+  var god : integer;
+  Assert(god>0);
+  var vis:= false;
+  if god mod 4 = 0 then
+  begin
+    vis:= true;
+    if god mod 100 = 0 then
+      if god mod 400 = 0 then
+        vis:= true
+      else vis:= false;
+  end;
+  {Функционал узнающи  год высокосныйы или нет}
+  var m : 0.0;
+  var s:= 0.0;
+  s:= 60*m;
+  {Функционал переводящий из минут в секунды}
+end.
